0938c7b7d4f401c5c14253a7238c04552dcca9ff
Dom Webber <dom.webber@hotmail.com>
2025-05-27 21:28:56 +0100

    Restructure setup for SQS incoming processing

--- packages/sqs/src/SQSIncomingQueue.ts
+++ packages/sqs/src/SQSIncomingQueue.ts
@@ -14,13 +14,25 @@ export interface SQSIncomingQueueConnectOptions {
 export default class SQSIncomingQueue implements IncomingQueueAdapter {
   public type = "sqs";
 
-  protected _consumer: Consumer | undefined;
+  protected _consumer: Consumer;
+  protected _callback?: IncomingQueueMessageListener;
 
   constructor(
     protected _client: AWS.SQS,
     protected _queueURL: string,
     protected _queueName: string,
-  ) {}
+  ) {
+    this._consumer = Consumer.create({
+      extendedAWSErrors: true,
+      queueUrl: this._queueURL,
+      sqs: this._client,
+      attributeNames: ["All"],
+      messageAttributeNames: ["All"],
+      batchSize: 10,
+      // waitTimeSeconds: 20,
+      handleMessage: async (message) => this._handleMessage(message),
+    });
+  }
 
   public static async connect(
     url: string,
@@ -35,72 +47,67 @@ export default class SQSIncomingQueue implements IncomingQueueAdapter {
   }
 
   public async healthcheck() {
-    if (!this._consumer?.status.isRunning) {
+    if (!this._consumer.status.isRunning) {
       throw new Error("AWS SQS Receiver unexpectedly disconnected");
     }
   }
 
   public async close() {
-    this._consumer?.stop();
+    this._consumer.stop();
+  }
+
+  protected async _handleMessage(
+    message: AWS.Message,
+  ): Promise<AWS.Message | void> {
+    if (!message.Body) {
+      throw new Error("Received message with no body");
+    }
+
+    const headers: Headers = {};
+    for (const [key, value] of Object.entries(
+      message.MessageAttributes ?? {},
+    )) {
+      if (value.StringValue) {
+        headers[key] = value.StringValue;
+        continue;
+      }
+
+      if (value.BinaryValue) {
+        headers[key] = value.BinaryValue.toString();
+        continue;
+      }
+
+      if (value.StringListValues) {
+        headers[key] = value.StringListValues;
+        continue;
+      }
+
+      if (value.BinaryListValues) {
+        headers[key] = value.BinaryListValues.map((buffer) =>
+          buffer.toString(),
+        );
+        continue;
+      }
+    }
+
+    await this._callback?.({
+      // raw: message,
+      accept: async () => {
+        await this._client.deleteMessage({
+          QueueUrl: this._queueURL,
+          ReceiptHandle: message.ReceiptHandle,
+        });
+      },
+      reject: async () => {},
+      message: {
+        headers,
+        body: Buffer.from(message.Body),
+      },
+    });
   }
 
   public async consume(callback: IncomingQueueMessageListener): Promise<void> {
-    this._consumer = Consumer.create({
-      extendedAWSErrors: true,
-      queueUrl: this._queueURL,
-      sqs: this._client,
-      attributeNames: ["All"],
-      messageAttributeNames: ["All"],
-      batchSize: 10,
-      // waitTimeSeconds: 20,
-      handleMessage: async (message) => {
-        if (!message.Body) {
-          throw new Error("Received message with no body");
-        }
-
-        const headers: Headers = {};
-        for (const [key, value] of Object.entries(
-          message.MessageAttributes ?? {},
-        )) {
-          if (value.StringValue) {
-            headers[key] = value.StringValue;
-            continue;
-          }
-
-          if (value.BinaryValue) {
-            headers[key] = value.BinaryValue.toString();
-            continue;
-          }
-
-          if (value.StringListValues) {
-            headers[key] = value.StringListValues;
-            continue;
-          }
-
-          if (value.BinaryListValues) {
-            headers[key] = value.BinaryListValues.map((buffer) =>
-              buffer.toString(),
-            );
-            continue;
-          }
-        }
-
-        await callback({
-          raw: message,
-          accept: async () => {
-            await this._client.deleteMessage({
-              QueueUrl: this._queueURL,
-              ReceiptHandle: message.ReceiptHandle,
-            });
-          },
-          reject: async () => {},
-          message: {
-            headers,
-            body: Buffer.from(message.Body),
-          },
-        });
-      },
-    });
+    this._callback = callback;
 
     this._consumer.on("error", (error) => {
       console.error("Queue error:", error);
