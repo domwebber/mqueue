183e03265943cece2e216522437c67c0688037d7
Dom Webber <dom.webber@hotmail.com>
2025-05-27 23:07:05 +0100

    Add export for outgoing rhea queue

--- packages/adapters/rhea/src/RheaQueue.ts
+++ packages/adapters/rhea/src/RheaQueue.ts
@@ -1,8 +1,9 @@
+import RheaIncomingQueue from "./RheaIncomingQueue.js";
 import RheaOutgoingQueue from "./RheaOutgoingQueue.js";
 
 export default class RheaQueue {
   private constructor() {}
 
   public static Outgoing = RheaOutgoingQueue;
-  public static Incoming = undefined;
+  public static Incoming = RheaIncomingQueue;
 }
