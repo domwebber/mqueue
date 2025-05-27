dff044b08c3e81ba9faf78ad02860eec7fa9289f
Dom Webber <dom.webber@hotmail.com>
2025-05-27 09:00:24 +0100

    Start amqplib adapter

---
+++ packages/amqplib/src/OutgoingQueue.ts
@@ -0,0 +1,57 @@
+import type { Channel, Options as AmqplibOptions } from "amqplib";
+import { connect } from "amqplib";
+import { OutgoingQueue, QueueMessage } from "@mqueue/queue";
+
+interface Options {
+  queueName: string;
+}
+
+export default class AmqplibOutgoingQueue implements OutgoingQueue {
+  public type = "amqplib";
+  public queueName: string;
+
+  constructor(
+    public client: Channel,
+    { queueName }: Options,
+  ) {
+    this.queueName = queueName;
+  }
+
+  public static async connect(
+    options: string | AmqplibOptions.Connect,
+    queueName: string,
+    { socketOptions }: { socketOptions: Parameters<typeof connect>[1] },
+  ) {
+    const connection = await connect(options, socketOptions);
+    const channel = await connection.createChannel();
+    return new this(channel, { queueName });
+  }
+
+  public async sendMessage(message: QueueMessage): Promise<void> {
+    const result = this.client.sendToQueue(this.queueName, message.body, {
+      contentType: "application/json",
+      contentEncoding: "binary",
+      headers: message.headers,
+    });
+
+    if (!result) {
+      throw new Error(`Message failed to send to ${this.type} queue`);
+      // throw OutgoingQueueMessageSendFailedError.caused();
+    }
+  }
+
+  public async healthcheck() {
+    await this.client
+      .assertQueue(this.queueName, {
+        durable: true,
+      })
+      .catch((error) => {
+        throw error;
+      });
+  }
+
+  public async close() {
+    await this.client.close();
+    // await this._channel.connection.close();
+  }
+}
