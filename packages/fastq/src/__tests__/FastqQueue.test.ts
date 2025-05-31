f21d805f39cf9781e2b336694b710faf01bd1810
Dom Webber <dom.webber@hotmail.com>
2025-05-31 11:04:26 +0100

    Add FastqQueue tests

---
+++ packages/fastq/src/__tests__/FastqQueue.test.ts
@@ -0,0 +1,89 @@
+import assert from "node:assert";
+import test, { describe } from "node:test";
+import FastqQueue from "../FastqQueue.js";
+import FastqIncomingQueue from "../FastqIncomingQueue.js";
+import FastqOutgoingQueue from "../FastqOutgoingQueue.js";
+
+const timeout = 180_000;
+describe("FastqQueue", { timeout }, () => {
+  describe("Queue sender connection", () => {
+    test("Should connect and disconnect", { timeout }, async () => {
+      // Arrange
+
+      // Act
+      const incoming = new FastqQueue.Incoming(1);
+      const connection = new FastqQueue.Outgoing(incoming.queue);
+      await connection.healthcheck();
+      await connection.close();
+
+      // Assert
+      await assert.rejects(connection.healthcheck);
+    });
+  });
+
+  describe("Healthchecking connection", { timeout }, () => {
+    let incoming: FastqIncomingQueue;
+    let outgoing: FastqOutgoingQueue;
+
+    test.before(
+      async () => {
+        incoming = new FastqQueue.Incoming(1);
+        outgoing = new FastqQueue.Outgoing(incoming.queue);
+      },
+      { timeout },
+    );
+
+    test.after(
+      async () => {
+        await outgoing.close();
+        await incoming.close();
+      },
+      { timeout },
+    );
+
+    test("Should succeed when healthchecked", { timeout }, async () => {
+      // Act
+      const result = await outgoing.healthcheck();
+
+      // Assert
+      assert.strictEqual(result, undefined);
+    });
+  });
+
+  describe("Sending messages", { timeout }, () => {
+    let incoming: FastqIncomingQueue;
+    let outgoing: FastqOutgoingQueue;
+
+    test.before(
+      async () => {
+        incoming = new FastqQueue.Incoming(1);
+        outgoing = new FastqQueue.Outgoing(incoming.queue);
+      },
+      { timeout },
+    );
+
+    test.after(
+      async () => {
+        await outgoing.close();
+        await incoming.close();
+      },
+      { timeout },
+    );
+
+    test("Should send a message", { timeout }, async () => {
+      // Arrange
+      const body = "This is a message";
+
+      // Act
+      const result = await outgoing.sendMessage({
+        headers: {
+          Example: "Example",
+        },
+        body: Buffer.from(body),
+      });
+
+      // Assert
+      assert.strictEqual(result, undefined);
+    });
+  });
+});
