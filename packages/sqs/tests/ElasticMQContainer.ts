0b05145e001c20000f4a0f5c003c12731cdab14e
Dom Webber <dom.webber@hotmail.com>
2025-06-19 08:46:46 +0100

    Add test cases for SQS with ElasticMQ

---
+++ packages/sqs/tests/ElasticMQContainer.ts
@@ -0,0 +1,82 @@
+import {
+  AbstractStartedContainer,
+  GenericContainer,
+  Wait,
+} from "testcontainers";
+import type { StartedTestContainer } from "testcontainers";
+
+export default class ElasticMQContainer extends GenericContainer {
+  public static REST_PORT = 9324;
+  public static UI_PORT = 9325;
+
+  constructor(
+    image = process.env.ELASTICMQ_IMAGE ||
+      "softwaremill/elasticmq-native:latest",
+  ) {
+    super(image);
+
+    this.withExposedPorts(
+      ElasticMQContainer.REST_PORT,
+      ElasticMQContainer.UI_PORT,
+    )
+      .withCopyContentToContainer([
+        {
+          content: [
+            "# the include should be done only once, at the beginning of the custom configuration file",
+            'include classpath("application.conf")',
+            "",
+            "queues {",
+            "  queue1 {",
+            "    defaultVisibilityTimeout = 10 seconds",
+            "    delay = 5 seconds",
+            "    receiveMessageWait = 0 seconds",
+            "    deadLettersQueue {",
+            '      name = "queue1-dead-letters"',
+            "      maxReceiveCount = 3 // from 1 to 1000",
+            "    }",
+            "    fifo = false",
+            "    contentBasedDeduplication = false",
+            '    copyTo = "audit-queue-name"',
+            '    moveTo = "redirect-queue-name"',
+            "    tags {",
+            '      tag1 = "tagged1"',
+            '      tag2 = "tagged2"',
+            "    }",
+            "  }",
+            "  queue1-dead-letters { }",
+            "  audit-queue-name { }",
+            "  redirect-queue-name { }",
+            "}",
+          ].join("\n"),
+          target: "/opt/elasticmq.conf",
+        },
+      ])
+      .withWaitStrategy(Wait.forLogMessage(/ElasticMQ server \(.*?\) started/))
+      .withStartupTimeout(60_000)
+      .withNetworkAliases("broker");
+  }
+
+  public override async start(): Promise<StartedElasticMQContainer> {
+    return new StartedElasticMQContainer(await super.start());
+  }
+}
+
+export class StartedElasticMQContainer extends AbstractStartedContainer {
+  public useAlias = false;
+
+  constructor(started: StartedTestContainer) {
+    super(started);
+  }
+
+  public getRestPort(): number {
+    return this.getMappedPort(ElasticMQContainer.REST_PORT);
+  }
+
+  public getUiPort(): number {
+    return this.getMappedPort(ElasticMQContainer.UI_PORT);
+  }
+
+  public getSQSUrl(): string {
+    return `http://${this.useAlias ? "activemq" : this.getHost()}:${this.getRestPort()}`;
+  }
+}
---
+++ packages/sqs/tests/SQSQueue.test.ts
@@ -0,0 +1,125 @@
+import assert from "node:assert";
+import test, { describe } from "node:test";
+import ElasticMQContainer, {
+  StartedElasticMQContainer,
+} from "./ElasticMQContainer.js";
+import SQSQueue from "../src/SQSQueue.js";
+import SQSOutgoingQueue from "../src/SQSOutgoingQueue.js";
+
+const timeout = 180_000;
+const url = "/queue1";
+const region = "elasticmq";
+const credentials = {
+  accessKeyId: "x",
+  secretAccessKey: "x",
+};
+
+describe("SQSQueue", { timeout }, () => {
+  let container: StartedElasticMQContainer;
+
+  test.before(
+    async () => {
+      container = await new ElasticMQContainer().start();
+    },
+    { timeout },
+  );
+
+  test.after(
+    async () => {
+      await container.stop({ timeout: 10_000 });
+    },
+    { timeout },
+  );
+
+  describe("Queue sender connection", () => {
+    test("Should connect and disconnect", { timeout }, async () => {
+      // Arrange
+
+      // Act
+      const connection = await SQSQueue.Outgoing.connect(url, {
+        clientConfig: {
+          credentials,
+          region,
+          endpoint: container.getSQSUrl(),
+        },
+      });
+      await connection.healthcheck();
+      await connection.close();
+
+      // Assert
+      await assert.rejects(connection.healthcheck);
+    });
+  });
+
+  describe("Healthchecking connection", { timeout }, () => {
+    let connection: SQSOutgoingQueue;
+
+    test.before(
+      async () => {
+        connection = await SQSQueue.Outgoing.connect(url, {
+          clientConfig: {
+            credentials,
+            region,
+            endpoint: container.getSQSUrl(),
+          },
+        });
+      },
+      { timeout },
+    );
+
+    test.after(
+      async () => {
+        await connection.close();
+      },
+      { timeout },
+    );
+
+    test("Should succeed when healthchecked", { timeout }, async () => {
+      // Act
+      const result = await connection.healthcheck();
+
+      // Assert
+      assert.strictEqual(result, undefined);
+    });
+  });
+
+  describe("Sending messages", { timeout }, () => {
+    let connection: SQSOutgoingQueue;
+
+    test.before(
+      async () => {
+        connection = await SQSQueue.Outgoing.connect(url, {
+          clientConfig: {
+            credentials,
+            region,
+            endpoint: container.getSQSUrl(),
+          },
+        });
+      },
+      { timeout },
+    );
+
+    test.after(
+      async () => {
+        await connection.close();
+      },
+      { timeout },
+    );
+
+    test("Should send a message", { timeout }, async () => {
+      // Arrange
+      const body = "This is a message";
+
+      // Act
+      const result = await connection.sendMessage({
+        headers: {
+          Example: "Example",
+        },
+        body: Buffer.from(body),
+      });
+
+      // Assert
+      assert.strictEqual(result, undefined);
+    });
+  });
+});
