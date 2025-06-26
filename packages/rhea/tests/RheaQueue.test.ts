0e99c4b2ebce5793a8b0f84ae6887ee8c33a29d2
Dom Webber <dom.webber@hotmail.com>
2025-06-26 22:29:17 +0100

    Add Rhea inbound queue assertion

--- packages/rhea/tests/RheaQueue.test.ts
+++ packages/rhea/tests/RheaQueue.test.ts
@@ -1,10 +1,12 @@
 import assert from "node:assert";
-import test, { describe } from "node:test";
+import test, { describe, mock } from "node:test";
 import RheaQueue from "../src/RheaQueue.js";
 import RabbitMQContainer, {
   StartedRabbitMQContainer,
 } from "./RabbitMQContainer.js";
 import RheaOutgoingQueue from "../src/RheaOutgoingQueue.js";
+import RheaIncomingQueue from "../src/RheaIncomingQueue.js";
+import { IncomingQueueMessageListenerInput } from "@mqueue/queue";
 
 const timeout = 180_000;
 describe("RheaQueue", { timeout }, () => {
@@ -72,12 +74,18 @@ describe("RheaQueue", { timeout }, () => {
 
   describe("Sending messages", { timeout }, () => {
     let connection: RheaOutgoingQueue;
+    let incoming: RheaIncomingQueue;
 
     test.before(
       async () => {
         connection = await RheaQueue.Outgoing.connect(
           container.getAmqpOptions(),
           { target: { address: container.getQueueName() } },
+        );
+
+        incoming = await RheaQueue.Incoming.connect(
+          container.getAmqpOptions(),
+          { source: { address: container.getQueueName() } },
         );
       },
       { timeout },
@@ -86,6 +94,7 @@ describe("RheaQueue", { timeout }, () => {
     test.after(
       async () => {
         await connection.close();
+        await incoming.close();
       },
       { timeout },
     );
@@ -93,8 +102,18 @@ describe("RheaQueue", { timeout }, () => {
     test("Should send a message", { timeout }, async () => {
       // Arrange
       const body = "This is a message";
+      const consumer = mock.fn<() => Promise<void>>();
 
       // Act
+      const receipt = new Promise<IncomingQueueMessageListenerInput>(
+        (resolve) => {
+          incoming.consume(async (payload) => {
+            await consumer();
+            resolve(payload);
+          });
+        },
+      );
+
       const result = await connection.sendMessage({
         headers: {
           Example: "Example",
@@ -102,8 +121,12 @@ describe("RheaQueue", { timeout }, () => {
         body: Buffer.from(body),
       });
 
+      const received = await receipt;
+
       // Assert
       assert.strictEqual(result, undefined);
+      assert.strictEqual(consumer.mock.calls.length, 1);
+      assert.equal(received.message.body.toString(), body);
     });
   });
 });
