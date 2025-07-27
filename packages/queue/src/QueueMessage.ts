15619d23a55cdeff8a710971c2e981ca984fbe52
Dom Webber <dom.webber@hotmail.com>
2025-07-27 12:01:24 +0100

    Move JSDOc comments to class from interface

--- packages/queue/src/QueueMessage.ts
+++ packages/queue/src/QueueMessage.ts
@@ -10,26 +10,14 @@ export interface QueueMessageOptions {
 
 export type QueueMessageFromJSONOptions = Omit<QueueMessageOptions, "body">;
 
-export interface QueueMessageType {
-  /** Whether the message was redelivered */
-  isRedelivered?: boolean;
+export class QueueMessage {
+  /** Body Content */
+  public body: Buffer;
 
   /** Message Headers */
-  headers: QueueMessageHeaders;
-
-  /** Body Content */
-  body: Buffer;
-
-  /** Body Content as JSON */
-  json<T = unknown>(): Promise<T>;
-
-  /** Body Content as Text/String */
-  text(): Promise<string>;
-}
-
-export class QueueMessage implements QueueMessageType {
-  public body: Buffer;
   public headers: QueueMessageHeaders;
+
+  /** Whether the message was redelivered */
   public isRedelivered?: boolean;
 
   constructor({ body, headers, isRedelivered = false }: QueueMessageOptions) {
@@ -38,11 +26,13 @@ export class QueueMessage implements QueueMessageType {
     this.isRedelivered = isRedelivered;
   }
 
+  /** Body Content as JSON */
   public async json<T = unknown>(): Promise<T> {
     const text = await this.text();
     return JSON.parse(text);
   }
 
+  /** Body Content as Text/String */
   public async text(): Promise<string> {
     return this.body.toString("utf8");
   }
